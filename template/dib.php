bef7ad2f53a8ae2f6deae335545273c166163b04
Leival <162357570+Leival@users.noreply.github.com>
2025-02-28 00:01:45 -0500

    Add files via upload

---
+++ template/dib.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Dibujo</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <?php  $url="http://".$_SERVER ['HTTP_HOST']."/artvisual" ?>
+
+    <nav class="navbar navbar-expand navbar-light bg-light">
+        <div class="nav navbar-nav">
+          <a class="nav-item nav-link" href="../welcome.php"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAMJJREFUSEvdldENwjAMRF83gU1gFCYBJgE2YRO6CehQUpUg69oo7UctVflIfc92ErtjYesW1md1wAE4A1pr7AlcAa1fKzN4Absa5ZGPxI8R4B2ApzL//MsMtg+4APoisyVytZZAnw5Ra2lNABKNIBaQf3CZZMijKFlTgCAn4D6KxgJc5FlAJZL48GKTYxPAz0stIrIA99BuKfLqa+oAU0s4dIjttYoW7Vo3bB+1aw0aHWTtTLADxx3i7P3VZ/LsCJ3DB77FMhmchYmbAAAAAElFTkSuQmCC"/>Salir</a>
+        </div>
+    </nav>
+
+  <div class="container">
+    <br>
+       <div class="row">
+
