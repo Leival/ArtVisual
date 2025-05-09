86260b3684baaed25d6bc3841cdbad4f1ddb59d3
Leival <162357570+Leival@users.noreply.github.com>
2025-05-08 20:48:58 -0500

    Add files via upload

---
+++ Database/art (1).sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-05-2025 a las 03:48:37
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `art`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `artistas`
+--
+-- Error leyendo la estructura de la tabla art.artistas: #1932 - Table 'art.artistas' doesn't exist in engine
+-- Error leyendo datos de la tabla art.artistas: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`artistas`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+-- Error leyendo la estructura de la tabla art.categorias: #1932 - Table 'art.categorias' doesn't exist in engine
+-- Error leyendo datos de la tabla art.categorias: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`categorias`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+-- Error leyendo la estructura de la tabla art.comentarios: #1932 - Table 'art.comentarios' doesn't exist in engine
+-- Error leyendo datos de la tabla art.comentarios: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`comentarios`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dibujo`
+--
+-- Error leyendo la estructura de la tabla art.dibujo: #1932 - Table 'art.dibujo' doesn't exist in engine
+-- Error leyendo datos de la tabla art.dibujo: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`dibujo`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `favoritos`
+--
+-- Error leyendo la estructura de la tabla art.favoritos: #1932 - Table 'art.favoritos' doesn't exist in engine
+-- Error leyendo datos de la tabla art.favoritos: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`favoritos`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fotografia`
+--
+-- Error leyendo la estructura de la tabla art.fotografia: #1932 - Table 'art.fotografia' doesn't exist in engine
+-- Error leyendo datos de la tabla art.fotografia: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`fotografia`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obras`
+--
+-- Error leyendo la estructura de la tabla art.obras: #1932 - Table 'art.obras' doesn't exist in engine
+-- Error leyendo datos de la tabla art.obras: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`obras`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pintura`
+--
+-- Error leyendo la estructura de la tabla art.pintura: #1932 - Table 'art.pintura' doesn't exist in engine
+-- Error leyendo datos de la tabla art.pintura: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`pintura`' en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+-- Error leyendo la estructura de la tabla art.users: #1932 - Table 'art.users' doesn't exist in engine
+-- Error leyendo datos de la tabla art.users: #1064 - Algo está equivocado en su sintax cerca 'FROM `art`.`users`' en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
